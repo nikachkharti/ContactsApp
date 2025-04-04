211c0b7c3d6cdfe0542bb3147032c6bb5e6e9b8c
Nikoloz Chkhartishvili <Nikoloz.Chkhartishvili@pashabank.ge>
2025-04-04 16:30:37 +0400

    Project initial

---
+++ ContactsApplication/ContactsApplication.API/Program.cs
@@ -0,0 +1,26 @@
+
+using ContactsApplication.API.Extensions;
+
+namespace ContactsApplication.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.AddControllers();
+            builder.AddOpenApi();
+            builder.AddSwagger();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
