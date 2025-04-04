b73e5afab4c60a13d8154ca1652be7d637f2f49c
Nikoloz Chkhartishvili <Nikoloz.Chkhartishvili@pashabank.ge>
2025-04-04 17:04:38 +0400

    minor

---
+++ ContactsApplication/ContactsApplication.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,22 @@
+﻿using ContactsApplication.Application.Contracts;
+using ContactsApplication.Infrastructure.Helper;
+using ContactsApplication.Infrastructure.Repository;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactsApplication.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Configure MongoDB settings
+            services.Configure<MongoDbSettings>(configuration.GetSection("MongoDB")); // TODO CONTINUE HERE
+
+            // Register repositories
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            return services;
+        }
+    }
+}
---
+++ ContactsApplication/ContactsApplication.Infrastructure/Middleware/DataSeedingMiddleware.cs
@@ -0,0 +1,70 @@
+﻿using ContactsApplication.Domain.Entities;
+using ContactsApplication.Infrastructure.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ContactsApplication.Infrastructure.Middleware
+{
+    public class DataSeedingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public DataSeedingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context, IServiceProvider serviceProvider)
+        {
+            try
+            {
+                using var scope = serviceProvider.CreateScope();
+                var personRepository = scope.ServiceProvider.GetRequiredService<PersonRepository>();
+
+                var existingPerson = await personRepository.GetAll(pageNumber: 1, pageSize: 1);
+                if (!existingPerson.Any())
+                {
+                    var people = new List<Person>
+                    {
+                        new Person()
+                        {
+                            Id = "35851DD7-D257-4B9B-85DC-818D0FC80E0C",
+                            Name = "John Lennon"
+                        },
+                        new Person()
+                        {
+                            Id = "5E6C4095-4B9A-49F3-990A-E7375FCEA90A",
+                            Name = "Paul Maccartney"
+                        },
+                        new Person()
+                        {
+                            Id = "9989BF8F-4FF6-4E51-8268-6FC8A8C0D22A",
+                            Name = "Ringo Starr"
+                        },
+                        new Person()
+                        {
+                            Id = "8A9600B6-1984-43FA-943C-C00AE4DA63CD",
+                            Name = "George Harrison"
+                        }
+                    };
+
+
+                    foreach (var person in people)
+                        await personRepository.Insert(person);
+
+                    //Log.Information("Database seeding completed.");
+                }
+                else
+                {
+                    //Log.Information("Database already contains data. Skipping seeding.");
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log.Error($"Data seeding failed: {ex.Message}");
+            }
+
+            await _next(context);
+        }
+    }
+}
