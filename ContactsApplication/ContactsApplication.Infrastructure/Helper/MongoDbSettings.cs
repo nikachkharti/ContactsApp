23d50e770011238bee7990e3de88aa1591a98086
Nikoloz Chkhartishvili <Nikoloz.Chkhartishvili@pashabank.ge>
2025-04-04 17:00:16 +0400

    Mongo helper

---
+++ ContactsApplication/ContactsApplication.Infrastructure/Helper/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace ContactsApplication.Infrastructure.Helper
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
