bfd62fb26f0fc6e2caf291b41f3991c70574baf9
Nikoloz Chkhartishvili <Nikoloz.Chkhartishvili@pashabank.ge>
2025-04-04 16:36:03 +0400

    Domain

---
+++ ContactsApplication/ContactsApplication.Domain/Entities/Person.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace ContactsApplication.Domain.Entities
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string Name { get; set; }
+    }
+}
