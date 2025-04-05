37e2b8f49cc7ad107d046e956cd84594808e1843
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-04-05 10:50:02 +0400

    GetAllPeople endpoint

---
+++ ContactsApplication/ContactsApplication.API/Controllers/PeopleController.cs
@@ -0,0 +1,25 @@
+﻿using ContactsApplication.Application.Contracts;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactsApplication.API.Controllers
+{
+    [Route("api/people")]
+    [ApiController]
+    public class PeopleController
+        (IPersonRepository personRepository)
+        : ControllerBase
+    {
+
+        /// <summary>
+        /// Get all people from database
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public async Task<IActionResult> GetAllPeople()
+        {
+            var result = await personRepository.GetAll();
+            return Ok(result);
+        }
+    }
+}
